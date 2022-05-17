4c7aab6ef967053cfb791b499af98524e7ef4d68
Mateusz Soroka <xmsoroka@gmail.com>
2022-05-17 18:36:46 +0200

    Add installation of predefined metadata

---
+++ src/PimcoreLocalizedAssetsBundle/Installer.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lemonmind\PimcoreLocalizedAssetsBundle;
+
+use Exception;
+use Pimcore\Extension\Bundle\Installer\SettingsStoreAwareInstaller;
+use Pimcore\Model\Metadata\Predefined;
+use Pimcore\Tool;
+
+class Installer extends SettingsStoreAwareInstaller
+{
+    /**
+     * @throws Exception
+     */
+    public function install(): void
+    {
+        $validLanguages = Tool::getValidLanguages();
+
+        foreach ($validLanguages as $validLanguage) {
+            $predefinedMeta = Predefined::getByName('localized_asset_name', $validLanguage);
+
+            if (!is_null($predefinedMeta)) {
+                continue;
+            }
+
+            $predefinedMeta = new Predefined();
+            $predefinedMeta->setName('localized_asset_name');
+            $predefinedMeta->setType('input');
+            $predefinedMeta->setLanguage($validLanguage);
+            $predefinedMeta->setGroup('Localized Assets Bundle');
+            $predefinedMeta->save();
+        }
+
+        $this->markInstalled();
+    }
+
+    public function uninstall(): void
+    {
+        $this->markUninstalled();
+    }
+}
--- src/PimcoreLocalizedAssetsBundle/PimcoreLocalizedAssetsBundle.php
+++ src/PimcoreLocalizedAssetsBundle/PimcoreLocalizedAssetsBundle.php
@@ -5,13 +5,25 @@ declare(strict_types=1);
 namespace Lemonmind\PimcoreLocalizedAssetsBundle;
 
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\Installer\InstallerInterface;
 
 class PimcoreLocalizedAssetsBundle extends AbstractPimcoreBundle
 {
-    public function getJsPaths()
+    public function getJsPaths(): array
     {
         return [
             '/bundles/pimcorelocalizedassets/js/pimcore/startup.js',
         ];
+    }
+
+    public function getInstaller(): InstallerInterface|null
+    {
+        $installerInstance = $this->container->get(Installer::class);
+
+        if ($installerInstance instanceof InstallerInterface) {
+            return $installerInstance;
+        }
+
+        return null;
     }
 }
