f0e38f56c94b9d6ad7c0bd2c1bb4c2a1ab5cec0c
Kacper Jabłoński <k.jablonski@lemonmind.com>
2023-12-18 14:04:40 +0100

    fix: add explode to ImageService

--- src/PimcoreLocalizedAssetsBundle/Services/ImageService.php
+++ src/PimcoreLocalizedAssetsBundle/Services/ImageService.php
@@ -9,7 +9,8 @@ class ImageService
     public static function generatePimcoreImageSrc(string $currentSrc, string $newFileName): string
     {
         $currentFileName = pathinfo($currentSrc, PATHINFO_FILENAME);
+        $test = explode('.' , $currentFileName);
 
-        return str_replace($currentFileName, $newFileName, $currentSrc);
+        return str_replace($test[0], $newFileName, $currentSrc);
     }
 }
